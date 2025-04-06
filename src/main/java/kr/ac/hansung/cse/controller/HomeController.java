ad0ad37087116286ebd86db0a619d447ce7c70ff
GridFin <tkfkgks200@gmail.com>
2025-04-06 16:42:12 +0900

    create helloSpringMVC application

--- src/main/java/kr/ac/hansung/cse/controller/HomeController.java
+++ src/main/java/kr/ac/hansung/cse/controller/HomeController.java
@@ -1,9 +1,10 @@
-package kr.ac.hansung.controller;
+package kr.ac.hansung.cse.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
